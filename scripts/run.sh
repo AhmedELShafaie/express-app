b511134c87498c46a6d62ea29fe0e7049159f1ec
Christophe Tafani-Dereeper <christophe.tafani-dereeper@epfl.ch>
2016-03-21 22:41:57 +0100

    make scripts executable

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source /home/ec2-user/.bash_profile
-npm=`which npm`
-echo "done. npm path = $npm" >> /home/ec2-user/HEYYOU
+u=$(whoami)
+echo "done. I am $u" >> /home/ec2-user/HEYYOU
 cd /home/ec2-user/app
 npm install
 forever start index.js
