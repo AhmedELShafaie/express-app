9420af349eb4ddfc55127a5a4003d057de1fd64c
Christophe Tafani-Dereeper <christophe.tafani-dereeper@epfl.ch>
2016-03-21 22:25:28 +0100

    make scripts executable

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+source /home/ec2-user/.bash_profile
 echo "done" >> /home/ec2-user/HEYYOU
 cd /home/ec2-user/app
 npm install
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app
 forever stopall
